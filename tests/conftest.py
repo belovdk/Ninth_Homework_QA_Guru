2ed304da8fb43c9ba5a45fc26dccbcef109a7a4a
Dmitriy Belov <belovdk@gmail.com>
2023-08-31 23:28:47 +0700

    Включил headless режим

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ from selenium import webdriver
 def browser_manager():
     browser.config.base_url = 'https://github.com'
     browser_option = webdriver.ChromeOptions()
-    #browser_option.add_argument('--headless')
+    browser_option.add_argument('--headless')
     browser_option.add_argument('window-size=1920,1080')
     browser.config.driver_options = browser_option
 
